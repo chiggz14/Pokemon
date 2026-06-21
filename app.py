652c98e9805d46721425bb5686a70124543e1a14
chiggz14 <chiggz14@gmail.com>
2026-06-21 02:11:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 import streamlit as st
 import pandas as pd
 
-# -----------------------------
-# PAGE CONFIG
-# -----------------------------
-st.set_page_config(page_title="Pokemon Dashboard", layout="wide")
-
+# -------------------------
+# PAGE TITLE
+# -------------------------
 st.title("📊 Pokemon Dashboard")
-# -----------------------------
+
+# -------------------------
 # LOAD DATA
-# -----------------------------
+# -------------------------
 @st.cache_data
 def load_data():
-    df = pd.read_excel("pokemon.csv")
+    df = pd.read_csv("pokemon.csv")
     return df
 
 df = load_data()
+
+# -------------------------
+# DISPLAY DATA
+# -------------------------
+st.subheader("Pokemon Data")
+st.dataframe(df)
