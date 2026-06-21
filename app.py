58e5f4a70d0ec32f72509f94b45080ca266317ef
chiggz14 <chiggz14@gmail.com>
2026-06-21 02:04:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,17 @@ import pandas as pd
 st.set_page_config(page_title="Pokemon Dashboard", layout="wide")
 
 st.title("📊 Pokemon Dashboard")
+# -----------------------------
+# LOAD DATA
+# -----------------------------
+@st.cache_data
+def load_data():
+    df = pd.read_excel("pokemon.xlsx")
+    
+    # Create combined type column
+    df["Type Combined"] = df["Type1"].fillna('') + " / " + df["Type2"].fillna('')
+    df["Type Combined"] = df["Type Combined"].str.replace(" / $", "", regex=True)
+    
+    return df
+
+df = load_data()
